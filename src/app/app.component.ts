fb187b906ad4e509b9d10dc20f9ad8c1b345b0e0
mercesolis <mersolis@live.com>
2020-09-07 21:42:47 -0500

    search and savetolist works!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component,OnInit } from '@angular/core';
+import { MovieService } from './services/movie.service';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,19 @@ import { Component,OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  ngOnInit(): void {
-   
-  }
   searchResults = [];
   myMovieList =  [];
   title = 'Angular Flix';
+
+  constructor(private movieService: MovieService) {
+
+  }
+
+  ngOnInit(): void {
+
+    this.searchResults = this.movieService.getSearchResults();
+    this.myMovieList = this.movieService.getMovieList();
+    this.movieService.loadMovieList();
+
+  }
 }
--- src/app/services/movie-api.service.ts
+++ src/app/services/movie-api.service.ts
@@ -6,13 +6,14 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 export class MovieAPIService {
 
-  private readonly URL = 'https://api.themoviedb.org/3/';
+  private readonly URL = 'https://api.themoviedb.org/3/search/multi';
   private readonly API_KEY = 'f2187dacfb405791e3765f885a8a5ed2';
   constructor(private httpClient: HttpClient) { }
 
-  get(path: string): Promise<any> {
+  get(searchTerm: string): Promise<any> {
     const params: HttpParams = new HttpParams()
-      .append('api_key', this.API_KEY);
-    return this.httpClient.get<any[]>(`${this.URL}${path}`, {params}).toPromise();
+      .append('api_key', this.API_KEY)
+      .append('query', searchTerm);
+    return this.httpClient.get<any[]>(this.URL, {params}).toPromise();
   }
 }
